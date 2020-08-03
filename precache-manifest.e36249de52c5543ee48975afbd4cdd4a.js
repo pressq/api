620bcb9b16c75127ea372b325ab821221386d894
André Martins <andre.martins.17.am@gmail.com>
2020-08-03 21:17:39 +0100

    Updates

---
+++ precache-manifest.e36249de52c5543ee48975afbd4cdd4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "030fcede20bdf183da6754d5f3f16e54",
+    "url": "/api/index.html"
+  },
+  {
+    "revision": "06553b2b8cdeb2de321d",
+    "url": "/api/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2c875111ddefc535db93",
+    "url": "/api/static/js/2.a3a17332.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/api/static/js/2.a3a17332.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06553b2b8cdeb2de321d",
+    "url": "/api/static/js/main.da80aec7.chunk.js"
+  },
+  {
+    "revision": "6fbb479d94dce2a9086d",
+    "url": "/api/static/js/runtime-main.294f9155.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/api/static/media/logo.5d5d9eef.svg"
+  }
+]);
